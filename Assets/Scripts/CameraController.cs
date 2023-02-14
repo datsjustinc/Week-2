16b348b06ba43bf84e2ebe5c0d8f8edc86128710
datsjustinc <jc9957@nyu.edu>
2023-02-13 22:50:54 -0500

    New Character/Animation/New Camera Switch Feature

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,16 +1,18 @@
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private GameObject[] players;
     [SerializeField] private List<Transform> playersTransform;
-    private Transform _currentPlayer;
+    private CinemachineVirtualCamera _camera;
     private int _currentPlayerIndex = 0;
 
     private void Start()
     {
-        _currentPlayer = null;
+        _camera = GetComponent <CinemachineVirtualCamera>();
+        
         players = GameObject.FindGameObjectsWithTag("Player");
 
         foreach (GameObject player in players)
@@ -23,11 +25,7 @@ public class CameraController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Tab))
         {
             _currentPlayerIndex = (_currentPlayerIndex + 1) % playersTransform.Count;
-            _currentPlayer = playersTransform[_currentPlayerIndex];
-            _currentPlayer.position = playersTransform[_currentPlayerIndex].position;
+            _camera.Follow = playersTransform[_currentPlayerIndex];
         }
-        
-        transform.position = _currentPlayer.position + Vector3.back * 3 + Vector3.up * 7;
-        transform.LookAt(_currentPlayer);
     }
 }
